968f4dcd691368797be190b33edb7561d26b3d83
Jiiiisu <Jiiiisu103526@gmail.com>
2024-10-04 23:52:46 +0900

    Update WelcomeScreen.js

--- FE/App/src/screens/WelcomeScreen.js
+++ FE/App/src/screens/WelcomeScreen.js
@@ -1,28 +1,38 @@
-import React, {useState} from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React, {useState, useEffect, useCallback, useRef} from 'react';
+import {View, Text, Image, TouchableOpacity, Animated} from 'react-native';
+import {useNavigation, useFocusEffect} from '@react-navigation/native';
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-import Logo from '../../assets/images/Logo.svg';
 
 export default function WelcomeScreen() {
   const navigation = useNavigation();
 
   const onboardingData = [
     {
-      image: require('../../assets/images/Peep(2-1).png'),
+      image: require('../../assets/images/Onboarding_First.png'),
       title: '환영합니다',
-      description: '우리 앱의 멋진 기능들을 만나보세요.',
+      description: '약 복용을 더욱 쉽게 도와드릴게요!',
     },
     {
-      image: require('../../assets/images/Peep(2-1).png'),
-      title: '쉽고 빠르게',
-      description: '간단한 조작으로 원하는 것을 빠르게 찾아보세요.',
+      image: require('../../assets/images/Onboarding_Camera.png'),
+      title: '알약 정보 검색',
+      description:
+        '이제 카메라로 알약을 찍어\n약 정보를 빠르게 검색할 수 있습니다.',
     },
     {
-      image: require('../../assets/images/Peep(1-1).png'),
+      image: require('../../assets/images/Onboarding_Alarm.png'),
+      title: '복용 일정 등록',
+      description: '정해진 시간에\n약을 잊지 않고 복용하세요!',
+    },
+    {
+      image: require('../../assets/images/Onboarding_Voice.png'),
+      title: '음성 명령',
+      description: '모든 기능을 음성 명령으로\n쉽게 이용해보세요.',
+    },
+    {
+      image: require('../../assets/images/Onboarding_Last.png'),
       title: '',
       description: '당신의 스마트 파트너,\n삐약삐약이 도와드릴게요!',
     },
@@ -30,9 +40,41 @@ export default function WelcomeScreen() {
 
   const [currentPage, setCurrentPage] = useState(0);
 
+  const fadeAnim = useRef(new Animated.Value(1)).current;
+
+  useEffect(() => {
+    setCurrentPage(0);
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      setCurrentPage(0);
+    }, []),
+  );
+
+  const fadeOut = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const fadeIn = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
   const handleNext = () => {
     if (currentPage < onboardingData.length - 1) {
-      setCurrentPage(currentPage + 1);
+      fadeOut();
+      setTimeout(() => {
+        setCurrentPage(currentPage + 1);
+        fadeIn();
+      }, 300);
     } else {
       navigation.navigate('Home');
     }
@@ -61,15 +103,16 @@ export default function WelcomeScreen() {
     <View className="flex-1 px-5 bg-default-1">
       {renderHeader()}
       <View className="flex-1 items-center justify-between p-5">
-        <Image
+        <Animated.Image
           source={onboardingData[currentPage].image}
-          style={{width: wp(100), height: hp(50)}}
+          style={{width: wp(80), height: hp(50), opacity: fadeAnim}}
           resizeMode="contain"
+          className="self-center"
         />
 
         <View>
           {onboardingData[currentPage].title && (
-            <Text className="text-gray-800 text-[28px] font-ExtraBold mt-8 mb-2 text-center">
+            <Text className="text-orange-default text-[28px] font-ExtraBold mt-8 mb-2 text-center">
               {onboardingData[currentPage].title}
             </Text>
           )}
